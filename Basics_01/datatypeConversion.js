7c447952d4298da107cd71042e1c6389c66cd46e
SummanDhar <sumandhar.vivo@gmail.com>
2024-07-01 00:43:31 +0530

    Operations

--- Basics_01/datatypeConversion.js
+++ Basics_01/datatypeConversion.js
@@ -18,3 +18,23 @@ let score = "33abc"
  let someValue = 33
  let TypeValue = String(someValue)
  console.log( typeof TypeValue);
+
+
+ // ***************************************** Operations ************************************
+
+ let value  = 3
+ let negValue = -value
+ console.log(negValue);
+ console.log(2%3)
+
+ let str1 = "Suman"
+ let str2 = " Dhar"
+
+ let str3 = str1+str2
+ console.log(str3);
+ console.log("1" +2); //output = 12
+ console.log("1" +2 +2);// output = 122
+ console.log(1 +2+"2");//output = 32
+// console.log("1" +2);
+console.log(+true);
+console.log((+""));
