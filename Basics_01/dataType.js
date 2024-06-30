3a19f68946654cb263e291c1e77561c40f03d002
SummanDhar <sumandhar.vivo@gmail.com>
2024-06-30 23:48:53 +0530

    first commit

---
+++ Basics_01/dataType.js
@@ -0,0 +1,22 @@
+console.log("Suman")
+console.log(2+3)
+//datatypes
+
+let name = "Alex"
+let age = 21
+let isLoggedIn = false
+console.log(name,age,isLoggedIn)
+
+//1.Premitive Datatype
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value 'type of null is object.'
+//undefined => 
+//symbol => unique 
+
+
+//object
+
+console.log(typeof null)
