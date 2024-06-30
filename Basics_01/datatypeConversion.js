c853eb92e8b1c0eb8c123e8534823ddcead9dd08
SummanDhar <sumandhar.vivo@gmail.com>
2024-07-01 00:10:56 +0530

    conversion of datatype

---
+++ Basics_01/datatypeConversion.js
@@ -0,0 +1,20 @@
+let score = "33abc"
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber )
+ console.log(valueInNumber )
+
+ //"33" => 33
+ //"33abc" => NaN
+ //true => 1; false => 0
+
+ let isLogged = 0
+ let booleanLogged = Boolean(isLogged)
+ console.log(booleanLogged)
+
+ // 1 => true ; 0 => false
+ //"" => false
+ //"suman" => True
+
+ let someValue = 33
+ let TypeValue = String(someValue)
+ console.log( typeof TypeValue);
